2cd787e8c0a65d7363d19d6c5b1beebdb07aec86
Prince Kumar Patel <148665134+hackerprince12@users.noreply.github.com>
2024-08-30 22:06:52 +0530

    url update

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4002", {
+      const socket = io("https://chatapp2-j0ol.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
